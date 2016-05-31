30b5f06dd4be3962d4142a20393e0d7d9091b3cd
Chen Sharon <chens@traiana.com>
2016-05-31 11:56:04 +0300

    adds basic skeleton framework for testing

---
+++ test/test_helper.js
@@ -0,0 +1,8 @@
+/**
+ * Created by chens on 5/31/2016.
+ */
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiImmutable).use(chaiAsPromised);
